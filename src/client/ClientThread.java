937bec063a2a6360f735a1257a84f0cd416ea0c5
Felipe Lohnefink <felipe.lohnefink@gmail.com>
2015-11-02 22:25:40 -0200

    Warning de Import resolvido antes de Enviar o Trabalho para a Professora

--- src/client/ClientThread.java
+++ src/client/ClientThread.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.net.SocketTimeoutException;
 
 public class ClientThread implements Runnable {
 
